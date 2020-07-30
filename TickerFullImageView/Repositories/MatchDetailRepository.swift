411cc994a3e616d8a497da298b6b14574732b4cd
Jacob <jacob_fafiolu@hotmail.co.uk>
2020-07-30 16:59:01 +0200

    [MatchDetail] - Added repository

---
+++ TickerFullImageView/Repositories/MatchDetailRepository.swift
@@ -0,0 +1,49 @@
+//
+//  MatchDetailRepository.swift
+//  TickerFullImageView
+//
+//  Created by user on 30/07/2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Foundation
+
+class MatchDetailRepository {
+    // MARK: State
+    let matchDetailImage: [MatchDetailImage]
+    var id: Int = 0
+    
+    // MARK: Properties
+    var previousMatchDetailImage: MatchDetailImage? { return getPreviousMatchDetailImage() }
+    var nextMatchDetailImage: MatchDetailImage? { return getNextMatchDetailImage() }
+    var currentMatchDetailImage: MatchDetailImage { return matchDetailImage[id] }
+    
+    init() {
+        matchDetailImage = [
+            MatchDetailImage("test.com", imageId: 0, imageCaption: "test0"),
+            MatchDetailImage("test.com", imageId: 1, imageCaption: "test1"),
+            MatchDetailImage("test.com", imageId: 2, imageCaption: "test2"),
+            MatchDetailImage("test.com", imageId: 3, imageCaption: "test3")
+        ]
+    }
+    
+    private func getNextMatchDetailImage() -> MatchDetailImage? {
+        if !isNextMatchDetailImageAvailable() { return nil }
+        id += 1
+        return matchDetailImage[id]
+    }
+    
+    public func getPreviousMatchDetailImage() -> Hero? {
+        if !isPreviousHeroAvailable() { return nil }
+        id -= 1
+        return matchDetailImage[id]
+    }
+    
+    public func isNextMatchDetailImageAvailable() -> Bool {
+        return matchDetailImage.count - 1 > id
+    }
+    
+    public func isPreviousMatchDetailImageAvailable() -> Bool {
+        return id > 0
+    }
+}
