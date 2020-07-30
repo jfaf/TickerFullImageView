7db0dc9f9847e3d3eeba88908de4ff257626d2c2
Jacob <jacob_fafiolu@hotmail.co.uk>
2020-07-30 16:35:00 +0200

    [MatchDetail] - Created match detail image model

---
+++ TickerFullImageView/Models/MatchDetailImage.swift
@@ -0,0 +1,25 @@
+//
+//  MatchDetailImage.swift
+//  TickerFullImageView
+//
+//  Created by user on 30/07/2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Foundation
+
+class MatchDetailImage {
+    
+    let imageUrl: String
+    let imageId: String
+    let imageCaption: String
+    
+    init(imageUrl: String, imageId: String, imageCaption: String) {
+        
+        self.imageUrl = imageUrl
+        self.imageId = imageId
+        self.imageCaption = imageCaption
+        
+    }
+    
+}
